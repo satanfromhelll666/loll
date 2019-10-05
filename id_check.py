8248fd6a88dd5e0e6614988727b64f844cb282e8
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2019-10-05 12:34:16 +0600

    Update id_check.py

--- id_check.py
+++ id_check.py
@@ -15,7 +15,7 @@ def id_check():
         if(arr[0]==0 and arr[1]==1 and arr[2]==1):
             if(arr[3]==0 or arr[3]==1 and arr[4]>=0 or arr[4] <=9 ):
                 if(arr[5]==1 or arr[5]==2 or arr[5]==3):
-                    if(arr[6]!=0 and arr[7]!=0 and arr[8]!=0):
+                    if(arr[6]!=0 or arr[7]!=0 or arr[8]!=0):
                         print('accepted')
                         return
 
